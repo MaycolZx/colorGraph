cf1d398b5e60d57b0d81ec8e376f8de445cdfbf5
Maycol-Zx-PC <alexander51600@hotmail.com>
2024-04-16 21:59:16 -0500

    draw

--- src/algo.cpp
+++ src/algo.cpp
@@ -1,7 +1,10 @@
 #include <GL/gl.h>
 #include <GLFW/glfw3.h>
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
+
+const float radiusG = 0.1f;
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
@@ -39,6 +42,82 @@ void drawLine(float x1, float y1, float x2, float y2) {
   glEnd();
 }
 
+void graphoCaseOne(float adjust) {
+  // Draw circles
+  drawCircle(-0.5f - adjust, 0.5f, radiusG, 1.0f, 0.0f,
+             0.0f); // Círculo rojo
+  drawCircle(0.5f - adjust * 2, 0.5f, radiusG, 0.0f, 1.0f,
+             0.0f); // Círculo verde
+  drawCircle(-0.5f - adjust, -0.5f, radiusG, 0.0f, 0.0f,
+             1.0f); // Círculo azul
+  drawCircle(0.5f - adjust * 2, -0.5f, radiusG, 1.0f, 0.0f, 0.0f);    //
+  drawCircle(0.5f + adjust, -0.5f + 0.5f, radiusG, 1.0f, 0.0f, 0.0f); //
+
+  // Draw lines between circles
+  glColor3f(0.5f, 0.5f, 0.5f); // Color de las líneas (gris)
+  // Línea del primer círculo al segundo círculo
+  // glBegin(GL_LINES);
+  drawLine(-0.3 - radiusG - adjust, 0.3 + 2 * radiusG, 0.3 - radiusG - adjust,
+           0.3 + 2 * radiusG);
+  // Línea del primer círculo al tercer círculo
+  drawLine(-0.3f - 2 * radiusG - adjust, 0.3f + radiusG,
+           -0.3f - 2 * radiusG - adjust, -0.3f - radiusG);
+  // Línea del segundo círculo al cuarto círculo
+  drawLine(0.3f - 2 * radiusG, 0.3f + radiusG, 0.3f - 2 * radiusG,
+           -0.3f - radiusG);
+  drawLine(-0.3f - radiusG - adjust, -0.3f - 2 * radiusG,
+           0.3f - radiusG - adjust, -0.3f - 2 * radiusG);
+  ///
+  drawLine(0.3f - 2 * radiusG, 0.3f + radiusG, 0.3f + radiusG + adjust,
+           -0.3f + radiusG + adjust);
+  drawLine(0.3f - 2 * radiusG, -0.3f - radiusG, 0.3f + radiusG + adjust,
+           -0.3f + radiusG + adjust);
+  //
+  drawLine(-0.3f - 2 * radiusG - adjust, 0.3f + radiusG,
+           0.3f + radiusG + adjust, -0.3f + radiusG + adjust);
+}
+
+void graphoCaseTwo(float adjust) {
+  //
+  drawCircle(-0.5f - adjust, 0.5f, radiusG, 1.0f, 0.0f,
+             0.0f); // Círculo rojo
+  drawCircle(0.5f - adjust * 2, 0.5f, radiusG, 0.0f, 1.0f,
+             0.0f); // Círculo verde
+  //
+  drawCircle(0.5f - radiusG, 0.5f, radiusG, 0.0f, 1.0f, 0.0f);
+  drawCircle(0.5f - radiusG, -0.5f, radiusG, 1.0f, 0.0f, 0.0f);
+  //
+  drawCircle(-0.5f - adjust, -0.5f, radiusG, 0.0f, 0.0f, 1.0f);
+  drawCircle(0.5f - adjust * 2, -0.5f, radiusG, 1.0f, 0.0f, 0.0f);
+  //
+  drawCircle(0.5f + adjust, -0.5f + 0.5f, radiusG, 1.0f, 0.75f, 0.50f);
+
+  glColor3f(0.5f, 0.5f, 0.5f);
+  // Línea del primer círculo al segundo círculo
+  drawLine(-0.3 - radiusG - adjust, 0.3 + 2 * radiusG, -0.3 - radiusG,
+           0.3 + 2 * radiusG);
+  // Línea del primer círculo al tercer círculo
+  drawLine(-0.3f - 2 * radiusG - adjust, 0.3f + radiusG,
+           -0.3f - 2 * radiusG - adjust, -0.3f - radiusG);
+  // Línea del segundo círculo al cuarto círculo
+  drawLine(-0.3f, 0.3f + radiusG, -0.3f, -0.3f - radiusG);
+  drawLine(-0.3f - radiusG - adjust, -0.3f - 2 * radiusG, -0.3f - radiusG,
+           -0.3f - 2 * radiusG);
+  drawLine(-0.3 + radiusG, 0.3 + 2 * radiusG, 0.3, 0.3 + 2 * radiusG);
+  //
+  drawLine(0.3f + radiusG, 0.3f + radiusG, 0.3f + radiusG, -0.3f - radiusG);
+  drawLine(0.3f, -0.3f - 2 * radiusG, 0.3f - radiusG - adjust,
+           -0.3f - 2 * radiusG);
+  ///
+  drawLine(0.3f + 2 * radiusG, 0.3f + 2 * radiusG, 0.3f + radiusG + adjust,
+           -0.3f - radiusG + adjust);
+  drawLine(0.3f + 2 * radiusG, -0.3f - 2 * radiusG, 0.3f + radiusG + adjust,
+           -0.3f - radiusG + adjust);
+  //
+  drawLine(-0.3f - 2 * radiusG - adjust, -0.3f - radiusG,
+           0.3f + radiusG + adjust, -0.3f + adjust - radiusG);
+}
+
 int main() {
   GLFWwindow *window;
 
@@ -56,26 +135,14 @@ int main() {
 
   glfwMakeContextCurrent(window);
 
+  int changeGraph = 0;
   while (!glfwWindowShouldClose(window)) {
     glClear(GL_COLOR_BUFFER_BIT);
-
-    // Draw circles
-    drawCircle(-0.5f, 0.5f, 0.2f, 1.0f, 0.0f, 0.0f);  // Círculo rojo
-    drawCircle(0.5f, 0.5f, 0.2f, 0.0f, 1.0f, 0.0f);   // Círculo verde
-    drawCircle(-0.5f, -0.5f, 0.2f, 0.0f, 0.0f, 1.0f); // Círculo azul
-    drawCircle(0.5f, -0.5f, 0.2f, 1.0f, 0.0f, 0.0f);  // Círculo amarillo
-
-    // Draw lines between circles
-    glColor3f(0.5f, 0.5f, 0.5f); // Color de las líneas (gris)
-    // Línea del primer círculo al segundo círculo
-    // glBegin(GL_LINES);
-    drawLine(-0.3, 0.3 + 0.2f, 0.3, 0.3 + 0.2f);
-    // Línea del primer círculo al tercer círculo
-    drawLine(-0.3f - 0.2f, 0.3f, -0.3f - 0.2f, -0.3f);
-    // Línea del segundo círculo al cuarto círculo
-    drawLine(0.3f + 0.2f, 0.3f, 0.3f + 0.2f, -0.3f);
-    drawLine(-0.3f, -0.3f - 0.2f, 0.3f, -0.3f - 0.2f);
-
+    if (changeGraph == 1) {
+      graphoCaseOne(0.2f);
+    } else {
+      graphoCaseTwo(0.4f);
+    }
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
